d12f9575de682d68c7215241c164876fd8e9b4f3
Thomas Packham <72440768+TPackham1@users.noreply.github.com>
2023-08-16 19:48:32 +0100

    move enemy class
    move enemy class to demo/MyGame.cpp as the other engines are not liking it, ie not coperating with it

--- engine/custom/MyEngineSystem.cpp
+++ engine/custom/MyEngineSystem.cpp
@@ -1,94 +1,42 @@
 #include "MyEngineSystem.h"
-#include <string>
-#include <cmath>
-#include <random>
 
-//#include "../GameMath.h"
+	
+// enemy class that take in 2Point to allow the enemy to 'see' the player when it updates
+// Enemy->Update is called when MyGame->Update is called, to keep them in sync
 
-
-enum class BehaviourStatus
-{
-	Success,
-	Failure,
-	Running
-};
-
-class Behaviour
-{
-public:
-	virtual BehaviourStatus Execute() = 0;
-};
-
-class Sequence : public Behaviour
-{
-public:
-	Sequence(const std::vector<Behaviour*>& Behaviours) : m_Behaviours(Behaviours) {}
-
-	BehaviourStatus Execute() override {
-		for (Behaviour* Behaviour : m_Behaviours) {
-			BehaviourStatus status = Behaviour->Execute();
-			if (status != BehaviourStatus::Success) {
-				return status;
-			}
-		}
-		return BehaviourStatus::Success;
-	}
-
-private:
-	std::vector<Behaviour*> m_Behaviours;
-};
-
-
-class MoveToPlayer : public Behaviour
-{
-public:
-	MoveToPlayer(MyEngineSystem* enemyBox) : m_Enemy(*enemyBox) {}
-
-	BehaviourStatus Execute() override {
-
-		if (IsPlayerInRange()) {
-			// Player is already in range, no need to move
-			return BehaviourStatus::Success;
-		}
-
-		// Move towards the player
-		MoveTowardsPlayer();
-
-		//  movement takes some time, returning 'Running' status
-		return BehaviourStatus::Running;
-	}
-private:
-	MyEngineSystem m_Enemy = new MyEngineSystem;
-
-	bool IsPlayerInRange() {
-		float distance = std::sqrt(m_Enemy.m_dx * m_Enemy.m_dx + m_Enemy.m_dy * m_Enemy.m_dy);
-		if (distance <= m_Enemy.m_attackRange)
-		{
-			std::cout << "Tom Packham CI517 2022" << std::endl;
-			return true;
-		}
-		else { return false; }
-	}
-
-	void MoveTowardsPlayer()
-	{
-		float distance = std::sqrt(m_Enemy.m_dx * m_Enemy.m_dx + m_Enemy.m_dy * m_Enemy.m_dy);
-		float directionX = m_Enemy.m_dx / distance;
-		float directionY = m_Enemy.m_dy / distance;
-		m_Enemy.m_x += directionX * m_Enemy.m_speed;
-		m_Enemy.m_y += directionY * m_Enemy.m_speed;
-	}
+//FUNCTIONS
+//Enemy::Enemy(Point2 startPos, int enemySpeed)
+//{
+//	debug("Enemy init");
+//	pos = startPos; speed = enemySpeed;
+//	
+//}
+//
+//	
+//
+//		void Enemy::Update(const Point2& playerPos) {
+//			// Calculate direction towards the player
+//			float dx = playerPos.x - pos.x;
+//			float dy = playerPos.y - pos.y;
+//			float distance = sqrt(dx * dx + dy * dy);
+//
+//			// Normalize direction vector
+//			if (distance != 0) {
+//				dx /= distance;
+//				dy /= distance;
+//			}
+//			//debug("Moving towards the player");
+//			// Move towards the player
+//		/*	pos.x += dx * speed;
+//			pos.y += dy * speed;
+//		*/
+//
+//
+//		}
 
 
 
-};
 
-//class AttackPlayer : public Behaviour {
-//public:
-//	BehaviourStatus Execute() override {
-//		
-//		return BehaviourStatus::Success;
-//	}
-//};
+
 
 
--- engine/custom/MyEngineSystem.h
+++ engine/custom/MyEngineSystem.h
@@ -1,13 +1,37 @@
 #ifndef __MY_ENGINE_H__
 #define __MY_ENGINE_H__
 
-#include "../EngineCommon.h"
 
-class MyEngineSystem {
+//
+class MyEngineSystem 
+{
 	friend class XCube2Engine;
 	private:
-
+//		/*Point2 currentPos;*/
+//		float speed;
+//	
 	public:
+		/*Point2 currentPos;
+		int speedx;*/
+		/*MyEngineSystem();*/
+		
+		
+		
+//
 };
 
-#endif
+//class Enemy
+//{
+//	public:
+//
+//		Enemy(Point2 startPos, int enemySpeed);
+//		void Update(const Point2& playerPos);
+//		
+//		/*float GetPosX();
+//		float GetPosY();*/
+//	private:
+//		Point2 pos;
+//		int speed;
+//};
+
+#endif // __MY_ENGINE_H__
